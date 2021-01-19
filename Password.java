1f224ab93a0085eba66153daf435e8c98f4058a1
bTsuyoi <gubbala.saikishore@gmail.com>
2021-01-19 12:02:01 +0530

    UPDATED: Password validation function to have alteast 1 number.

--- Password.java
+++ Password.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Password {
 	public static Scanner scanner = new Scanner(System.in);
 	public static void toCheckEmail() {
@@ -57,7 +60,7 @@ public class Password {
 		String password;
 		System.out.println("Enter password");
 		password = scanner.next();
-		if (Pattern.matches("^(?=.*[A-Z])[\\da-zA-Z]{8,}", password)) {
+		if (Pattern.matches("^(?=.*[A-Z])(?=.*[0-9])[\\da-zA-Z]{8,}", password)) {
 			System.out.println("Valid password.");
 		}
 		else {
