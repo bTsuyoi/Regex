ead456f45748345b353e02e0878f49ee527439a7
bTsuyoi <gubbala.saikishore@gmail.com>
2021-01-19 11:35:33 +0530

    Added: Functionality to check if the first name starts with caps.

--- FirstName.java
+++ FirstName.java
@@ -1,21 +1,28 @@
+
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class FirstName{
+public class FirstName {
+	public static Scanner scanner = new Scanner(System.in);
+	public static void toCheckFirstName() {
+		
+	       String firstName;        
+	       System.out.println("Enter First Name: ");
+	       firstName = scanner.next();
+	       
+	       if (Pattern.matches("[A-Z]{1}[a-zA-Z]{2,}$", firstName)) {
+	           System.out.println("Valid First Name");
+	       }
+	       else {
+	          System.out.println("Invalid First Name");
+	       }
+	       
+	}
 
-	public static void main(String[] args){
-
-		Scanner scanner = new Scanner(System.in);
-		String firstName;
-		System.out.println("Welcome to User Registration!");
-		System.out.println("Enter First Name: ");
-		firstName = scanner.next();
-		scanner.close();
-		if (Pattern.matches("[A-Z]{1}([a-zA-Z]){2,}$", firstName)) {
-			System.out.println("Valid First Name");
-		}
-		else {
-			System.out.println("Invalid First Name");
-		}
+	public static void main(String[] args) {
+		
+	   	toCheckFirstName();
+    	scanner.close();
+    	
 	}
 }
